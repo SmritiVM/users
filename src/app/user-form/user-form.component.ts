d520400db7bdb83ab96537a220aceac85be509c2
Smriti Vipin Madangarli <smritivipin.madangarli2021@vitstudent.ac.in>
2024-01-04 18:30:28 +0530

    remove attempt comments

--- src/app/user-form/user-form.component.ts
+++ src/app/user-form/user-form.component.ts
@@ -21,38 +21,7 @@ import { Company } from '../user';
 })
 export class UserFormComponent {
   user: User = {} as User;
-  // 
-  // user =  new User(0, '', '', '', {}, '', '', {});
-  // let geo_:  Geo = {
-  //       lat: '',
-  //       lng: '',
-  // }
-  // let address_ : Address = {
-  //     street: '',
-  //     suite: '',
-  //     city: '',
-  //     zipcode: '',
-  //     geo: this.geo_,
-  // }
-
-  // company_ = (): Company => ({
-  //     name: '',
-  //     catchPhrase: '',
-  //     bs: '',
-  // })
-  // user = (): User => ({
-  //   id: 0,
-  //   name: '',
-  //   username: '',
-  //   email: '',
-  //   address: this.address_,
-  //   phone: '',
-  //   website: '',
-  //   company: this.company_,
-  // }) 
-  // user: User[] = [];
-  // let user:  User = {};
-  
+    
   submitted = false;
   onSubmit() {this.submitted = true;
   console.log(this.user.name);
@@ -60,8 +29,6 @@ export class UserFormComponent {
   console.log(this.user.email);
   console.log(this.user.phone);
   console.log(this.user.website);
-  // console.log(this.user.address.street);
-  
 
   }
 
