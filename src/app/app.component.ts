95b9e93eae7d07ec2248e02c843d5c8624cccddd
Smriti Vipin Madangarli <smritivipin.madangarli2021@vitstudent.ac.in>
2024-02-10 18:49:12 +0530

    Revert "attempt at routing"
    This reverts commit cc1967bcf9d2d0d741316664381dcf6c54b0e1a3.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,14 +2,11 @@ import { Component } from '@angular/core';
 
 import { UserDetailsComponent } from './user-details/user-details.component';
 import { UserFormComponent } from './user-form/user-form.component';
-import { UserComponent } from './user/user.component';
-import { CommonModule } from '@angular/common';
-import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   standalone: true,
   selector: 'app-root',
-  imports: [UserDetailsComponent, UserFormComponent, CommonModule, RouterOutlet, RouterLink, RouterLinkActive, UserComponent],
+  imports: [UserDetailsComponent, UserFormComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,3 @@
 import { Routes } from '@angular/router';
-import { UserDetailsComponent } from './user-details/user-details.component';
-import { UserComponent } from './user/user.component';
-import { UserFormComponent } from './user-form/user-form.component';
 
-export const routes: Routes = [
-    {path: 'user-details', component: UserDetailsComponent},
-    {path: 'user/:id', component: UserComponent},
-    {path: 'user-form', component: UserFormComponent}
-];
+export const routes: Routes = [];
